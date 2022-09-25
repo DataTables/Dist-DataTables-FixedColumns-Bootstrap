780df9dc62845167172983ef0990d99a732618ef
Allan Jardine <allan@datatables.net>
2022-09-25 15:47:39 +0000

    Sync DistRepos

--- types/fixedColumns.bootstrap.d.ts
+++ types/fixedColumns.bootstrap.d.ts
@@ -1,7 +1,3 @@
 /*! Bootstrap integration for DataTables' FixedColumns
- * ©2016 SpryMedia Ltd - datatables.net/license
+ * © SpryMedia Ltd - datatables.net/license
  */
-declare let define: {
-    (stringValue: any, Function: any): any;
-    amd: string;
-};
