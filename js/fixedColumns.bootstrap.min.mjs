627ae93bd92b9f35e09c01e70ce32913563327fd
dtbuild <dtbuild@rigil>
2026-09-25 11:23:25 +0000

    eed35eb774935867b361bd89a58eaa62cc64ab21 Release 6.1.1
    Sync to source repo @eed35eb774935867b361bd89a58eaa62cc64ab21

--- js/fixedColumns.bootstrap.min.mjs
+++ js/fixedColumns.bootstrap.min.mjs
@@ -1,4 +1,4 @@
-/*! FixedColumns Bootstrap 3 styling 6.1.0 for DataTables
+/*! FixedColumns Bootstrap 3 styling 6.1.1 for DataTables
  * Copyright (c) SpryMedia Ltd - datatables.net/license
  */
 import DataTable from"datatables.net-bs";import FixedColumns from"datatables.net-fixedcolumns";export default DataTable;
